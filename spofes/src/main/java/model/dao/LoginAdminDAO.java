b8f1c98124982d72879d74c10d99f5fdcd4ae4b0
emtech-user <emtech-user@192.168.128.111>
2023-05-26 17:10:29 +0900

    阿部5/26

---
+++ spofes/src/main/java/model/dao/LoginAdminDAO.java
@@ -0,0 +1,38 @@
+package model.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import model.entity.SpoFesBean;
+
+public class LoginAdminDAO {
+
+	public boolean login(String id,String pass)throws ClassNotFoundException, SQLException {
+
+		String sql = "SELECT * FROM m_user WHERE user_id = ? AND password = ?";
+
+		// データベースへの接続の取得、PreparedStatementの取得
+		try(Connection con = ConnectionManager.getConnection();
+				PreparedStatement pstmt =  con.prepareStatement(sql)){
+
+			// プレースホルダへの値の設定
+			pstmt.setString(1, id);
+			pstmt.setString(2, pass);
+
+			// SQLステートメントの実行
+			ResultSet res = pstmt.executeQuery();
+			
+			SpoFesBean bean = new SpoFesBean();
+			bean.setName("name");
+			
+
+			// 結果の操作
+			if(res.next()) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
---
+++ spofes/src/main/webapp/adminlogin.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>ログイン</title>
+</head>
+<body>
+『Spofes』を始めよう！
+<form action="admin-login-servlet" method="POST">
+ID<br>
+<input type="text" name="id"><br>
+パスワード<br>
+<input type="password" name="pass"><br>
+<input type="submit" value="ログイン"><br>
+</form>
+</body>
+</html>
